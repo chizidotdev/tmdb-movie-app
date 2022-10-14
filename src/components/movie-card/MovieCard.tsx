b38530341a6a9c22a48a8a2291b804ce62c3e377
chiziivictor <chizivwokoma@gmail.com>
2022-10-14 06:19:13 +0100

    feat: transfer MovieCard component to typescript

---
+++ src/components/movie-card/MovieCard.tsx
@@ -0,0 +1,34 @@
+import './movie-card.scss';
+
+import { Link } from 'react-router-dom';
+
+import Button from '../button/Button';
+
+import { category } from '../../api/tmdbApi';
+import apiConfig from '../../api/apiConfig';
+
+interface MovieCardProps {
+  item: any;
+  category: string;
+}
+
+const MovieCard = (props: MovieCardProps) => {
+  const item = props.item;
+
+  const link = '/' + category[props.category] + '/' + item.id;
+
+  const bg = apiConfig.w500Image(item.poster_path || item.backdrop_path);
+
+  return (
+    <Link to={link}>
+      <div className='movie-card' style={{ backgroundImage: `url(${bg})` }}>
+        <Button>
+          <i className='bx bx-play'></i>
+        </Button>
+      </div>
+      <h3>{item.title || item.name}</h3>
+    </Link>
+  );
+};
+
+export default MovieCard;
