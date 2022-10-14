60b8bcb5d45bed78b826d71fea0a1e96563466eb
chiziivictor <chizivwokoma@gmail.com>
2022-10-14 06:17:56 +0100

    feat: transfer Input component to typescript

---
+++ src/components/input/Input.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './input.scss';
+
+interface InputProps extends React.ComponentPropsWithRef<'input'> {}
+
+const Input = (props: InputProps) => {
+  return <input />;
+};
+
+export default Input;
