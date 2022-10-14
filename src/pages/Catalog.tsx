f9e61b194a698ebc2f5acc9b55114d27b5c9115c
chiziivictor <chizivwokoma@gmail.com>
2022-10-14 06:36:52 +0100

    feat: transfer Catalog component to typescript

--- src/pages/Catalog.tsx
+++ src/pages/Catalog.tsx
@@ -13,7 +13,7 @@ const Catalog = () => {
       <PageHeader>{category === cate.movie ? 'Movies' : 'TV Series'}</PageHeader>
       <div className='container'>
         <div className='section mb-3'>
-          <MovieGrid category={category} />
+          <MovieGrid category={category!} />
         </div>
       </div>
     </>
